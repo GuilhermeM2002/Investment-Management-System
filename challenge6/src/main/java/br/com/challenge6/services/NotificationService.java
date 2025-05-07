8bed9ca9eb63ffeda2c5706bae232d9acce60620
GuilhermeM2002 <guilherme.infor2@gmail.com>
2025-05-06 21:26:14 -0300

    add: send email method

---
+++ challenge6/src/main/java/br/com/challenge6/services/NotificationService.java
@@ -0,0 +1,57 @@
+package br.com.challenge6.services;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class NotificationService {
+    @Value("${email.username}")
+    private String username;
+
+    @Value("${email.password}")
+    private String password;
+
+    @Value("${email.smtp.host}")
+    private String smtpHost;
+
+    @Value("${email.smtp.port}")
+    private String smtpPort;
+
+    @Value("${email.smtp.auth}")
+    private boolean smtpAuth;
+
+    @Value("${email.smtp.starttls.enable}")
+    private boolean startTls;
+
+    public void sendEmail(String to, String subject, String content) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.starttls.enable", startTls);
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", smtpPort);
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Erro ao enviar e-mail: " + e.getMessage(), e);
+        }
+    }
+}
