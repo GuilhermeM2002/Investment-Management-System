83366fa23ff680e6ac94b9fb3bc9ae643a227b71
GuilhermeM2002 <guilherme.infor2@gmail.com>
2025-09-17 12:04:18 -0300

    update: change AddInvestmentDTO and GetInvestmentDTO from record to class

--- challenge6/src/main/java/br/com/challenge6/domain/investment/AddInvestmentDTO.java
+++ challenge6/src/main/java/br/com/challenge6/domain/investment/AddInvestmentDTO.java
@@ -1,15 +1,24 @@
 package br.com.challenge6.domain.investment;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record AddInvestmentDTO(
-        Long id,
-        Long userId,
-        String assetType,
-        String ticker,
-        Double quantity,
-        Double buyPrice,
-        Double currentPrice,
-        LocalDate date
-) {
+import java.time.LocalDate;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AddInvestmentDTO{
+    private UUID id;
+    private Long userId;
+    private String assetType;
+    private String ticker;
+    private Double quantity;
+    private Double buyPrice;
+    private Double currentPrice;
+    private LocalDate date;
 }
